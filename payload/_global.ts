92079c8659e7f5641f77aa6c81fa16260d68adfb
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-06-08 21:05:33 +0900

    resume - last update

--- payload/_global.ts
+++ payload/_global.ts
@@ -1,5 +1,5 @@
 // import favicon from '../asset/favicon.ico';
-import previewImage from '../asset/preview.jpg';
+import previewImage from '../asset/preview_resume.png';
 import { IGlobal } from '../component/common/IGlobal';
 
 const title = 'RESUME';
